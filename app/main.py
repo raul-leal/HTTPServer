6f397ec532607fb64d2a6cc736e938aab61fabe8
Raul Leal <rhla19@hotmail.com>
2024-05-27 14:10:36 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 import socket
 
+def handle_request(client_socket):
+    client_socket.recv(1024)
+    response = "HTTP/1.1 200 OK\r\n\r\n"
+    client_socket.send(response.encode())
 
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()[0].sendall("HTTP/1.1 200 OK\r\n\r\n")
+    server_socket.accept()
 
 if __name__ == "__main__":
     main()
