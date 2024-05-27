1a69dab139018dfc05df39a4b2f4a8272ce6889c
Raul Leal <rhla19@hotmail.com>
2024-05-27 15:50:18 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -3,15 +3,15 @@ import socket
 def parse_request(data):
     lines = data.split('\r\n')
     start_line = lines[0]
-    method, path, version = start_line.split(' ')
-    return method, path, version
+    method, path, version, test = start_line.split(' ')
+    return method, path, version, test
 
 def get_response(path):
     if path.startswith('/echo'):
         path_message = path.split('/')[2]
         return f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path_message)}\r\n\r\n{path_message}'
     elif path.startswith('/user-agent'):
-        path_user_agent = path[12:]
+        path_user_agent = "placeholder"
         return f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path_user_agent)}\r\n\r\n{path_user_agent}'
     elif path == '/':
         return 'HTTP/1.1 200 OK\r\n\r\n'
@@ -21,8 +21,8 @@ def get_response(path):
 def handle_request(client_socket):
     data = client_socket.recv(1024)
     print(data)
-    method, path, version = parse_request(data.decode())
-    print(f'Method: {method}, path: {path}, version: {version}')
+    method, path, version, test = parse_request(data.decode())
+    print(f'Method: {method}, path: {path}, version: {version}, test: {test}')
     response = get_response(path)
     client_socket.sendall(response.encode())
 
