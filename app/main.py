4e8a695ebe4a684748e3f082086b3b46a1b04897
Raul Leal <rhla19@hotmail.com>
2024-05-27 14:20:17 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def main():
     print("Server is listening on port 4221...")
     client_socket, addr = server_socket.accept()
     print(f"Connection from {addr} has been established.")
-    client_socket.sendall(b"HTTP/1.1 200 OK \r\n\r\n")
+    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
     client_socket.close()
 
 
