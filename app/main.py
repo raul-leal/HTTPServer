8a239325df09bcd5d86b79d7de90aaffc20b37f9
Raul Leal <rhla19@hotmail.com>
2024-05-27 14:17:50 -0300

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,15 +1,13 @@
 import socket
 
-def handle_request(client_socket):
-    client_socket.recv(1024)
-    response = "HTTP/1.1 200 OK\r\n\r\n"
-    client_socket.send(response.encode())
-
 def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     print("Server is listening on port 4221...")
-    server_socket.accept()
-    handle_request(server_socket)
+    client_socket, addr = server_socket.accept()
+    print(f"Connection from {addr} has been established.")
+    client_socket.sendall(b"HTTP/1.1 200 OK \r\n\r\n")
+    client_socket.close()
+
 
 if __name__ == "__main__":
     main()
